52ec5a7a13231bea10ff226daf7a18c40ea7a431
Stas <leshuksu@gmail.com>
2023-01-18 23:36:31 +0300

    Played with Root Layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,5 @@
 	let title = 'Hello@';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<hr />
-
-<h2>{title}</h2>
+<h1>{title}</h1>
 <p>You can see page <a href="/about">"About"</a></p>
