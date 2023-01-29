cec881c2704497635afab91e7b056f34e1a234ea
Stas <leshuksu@gmail.com>
2023-01-29 18:42:32 +0300

    Played with SCSS and TS, installed dart-scss globally

--- src/routes/ts-section/+page.svelte
+++ src/routes/ts-section/+page.svelte
@@ -1,4 +1,24 @@
+<script lang="ts">
+	const output: string = `Hello world!`;
+</script>
+
 <h1>This is TypeScript Playground section</h1>
+<code class="output">
+	{output}
+</code>
 <hr />
 
 <p>Back to <a href="/">Root Page</a></p>
+
+<style lang="scss">
+	.output {
+		display: block;
+		padding: 10px;
+		width: 75%;
+		border: solid 2px black;
+		border-radius: 5px;
+		background-color: #2f677a;
+		color: lightcyan;
+		font-size: 18px;
+	}
+</style>
